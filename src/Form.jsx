b2592345cad4ac5cd3874a58ac0fb1ec79b8455a
badr ghanbi <bghanbi50@gmail.com>
2022-12-07 14:24:04 +0100

    create the list componenet

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 import InputItem from './InputItem';
 function Form(props) {
     return (
+        <>
         <form className='form'>
             <InputItem label='UserName'/>
             <InputItem label='Age'/>
+            <button>Add User</button>
         </form>
+        <ul className='Userlist'>
+            <li>max 31 year</li>
+            <li>max 31 year</li>
+        </ul>
+
+        </>
     );
 }
 
